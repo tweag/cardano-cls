7d62da1de3a35472e865b98171547eb420a8e8a5
João Santos Reis <joao.reis@tweag.io>
2026-01-14 17:39:51 +0000

    Refactor validate-scls-test integration in flake.nix and remove validate-scls-test package

--- nix/pkgs/validate-scls-test.nix
+++
@@ -1,8 +0,0 @@
-{ writeShellApplication, verify-scls, scls-util-exe }:
-writeShellApplication {
-  name = "validate-scls-test";
-  runtimeInputs = [ verify-scls scls-util-exe ];
-  text = ''
-    TEMP_DIR=$(mktemp -d) && scls-util debug generate "$TEMP_DIR/1.scls" && verify-scls "$TEMP_DIR/1.scls"
-  '';
-}
