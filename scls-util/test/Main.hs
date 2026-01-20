e7c1b54ec411c1406ba457082cbe6d444534bde2
João Santos Reis <joao.reis@tweag.io>
2026-01-20 14:31:05 +0000

    Refactor scls-util test specifications to accept optional SCLS utility path as env variable

--- scls-util/test/Main.hs
+++ scls-util/test/Main.hs
@@ -6,16 +6,19 @@ import ChecksumSpec
 import InfoSpec
 import MergeSpec
 import SplitSpec
+import System.Environment (lookupEnv)
 import Test.Hspec
 import VerifySpec
 
 main :: IO ()
-main = hspec $ do
-  describe "scls-util binary tests" do
-    checksumCommandTests
-    verifyNsCommandTests
-    infoCommandTests
-    listNsCommandTests
-    splitCommandTests
-    mergeCommandTests
-    verifyCommandTests
+main = do
+  sclsUtilPath <- lookupEnv "SCLS_UTIL_PATH"
+  hspec $
+    describe "scls-util binary tests" do
+      checksumCommandTests sclsUtilPath
+      verifyNsCommandTests sclsUtilPath
+      infoCommandTests sclsUtilPath
+      listNsCommandTests sclsUtilPath
+      splitCommandTests sclsUtilPath
+      mergeCommandTests sclsUtilPath
+      verifyCommandTests sclsUtilPath
--- scls-util/test/VerifySpec.hs
+++ scls-util/test/VerifySpec.hs
@@ -12,21 +12,21 @@ import System.FilePath ((</>))
 import System.IO.Temp (withSystemTempDirectory)
 import Test.Hspec
 
-verifyCommandTests :: Spec
-verifyCommandTests = describe "verify command" do
+verifyCommandTests :: Maybe FilePath -> Spec
+verifyCommandTests mSclsUtil = describe "verify command" do
   it "verify command verifies a valid SCLS file" do
     withSystemTempDirectory "scls-util-test-XXXXXX" \dir -> do
       -- arrange
-      (generateExitCode, _stdout, _) <- runSclsUtil ["debug", "generate", dir </> "output.scls", "--namespace", "utxo/v0:10"]
+      (generateExitCode, _stdout, _) <- runSclsUtil mSclsUtil ["debug", "generate", dir </> "output.scls", "--namespace", "utxo/v0:10"]
       generateExitCode `shouldBe` ExitSuccess
       -- act
 
-      (verifyExitCode, _stdout, _) <- runSclsUtil ["checksum", dir </> "output.scls"]
+      (verifyExitCode, _stdout, _) <- runSclsUtil mSclsUtil ["checksum", dir </> "output.scls"]
 
       -- assert
       verifyExitCode `shouldBe` ExitSuccess
 
   it "verify command fails for non-existent file" do
-    (exitCode, _, _) <- runSclsUtil ["verify", "/nonexistent/file.scls"]
+    (exitCode, _, _) <- runSclsUtil mSclsUtil ["verify", "/nonexistent/file.scls"]
 
     exitCode `shouldBe` ExitFailure 1
