25e355784ba1abf6a2788d22e2bdb2b482279d45
João Santos Reis <joao.reis@tweag.io>
2026-01-26 10:08:02 +0000

    Add update-reference script to manage CIPs input and Haskell CI updates

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -4,4 +4,5 @@ final: prev: {
   verify-scls =
     final.callPackage ./verify-scls.nix { inherit referenceSpecKsy; };
   validate-scls = final.callPackage ./validate-scls.nix { };
+  update-reference = final.callPackage ./update-reference.nix { };
 }
