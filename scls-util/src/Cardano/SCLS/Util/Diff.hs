ccb10031e44518d8f0860f7e4f53eacadf4a7553
João Santos Reis <joao.reis@tweag.io>
2026-02-03 09:52:32 +0000

    Refactor diff creation to utilize new DiffEntry and Pretty modules

--- scls-util/src/Cardano/SCLS/Util/Diff.hs
+++ scls-util/src/Cardano/SCLS/Util/Diff.hs
@@ -14,33 +14,31 @@ module Cardano.SCLS.Util.Diff (
 ) where
 
 import Cardano.SCLS.CDDL
-
 import Cardano.SCLS.Internal.Entry.ChunkEntry (ChunkEntry (..))
 import Cardano.SCLS.Internal.Reader (extractNamespaceList, namespacedData)
+import Cardano.SCLS.Util.Diff.Entry (DiffEntry (..))
+import Cardano.SCLS.Util.Diff.Pretty (ppEditDiffEntry)
+import Cardano.SCLS.Util.Diff.TermDiff (termToTree)
 import Cardano.SCLS.Util.Result
 import Cardano.Types.Namespace (Namespace)
 import Cardano.Types.Namespace qualified as Namespace
-import Codec.CBOR.Pretty (prettyHexEnc)
+import Codec.CBOR.FlatTerm (toFlatTerm)
 import Codec.CBOR.Term (encodeTerm)
 import Control.Monad (when)
 import Control.Monad.IO.Class (MonadIO (..))
 import Control.Monad.Logger
 import Control.Monad.Trans.Resource (MonadUnliftIO, allocate, runResourceT)
 import Data.ByteString (ByteString)
-import Data.ByteString.Base16 qualified as Base16
 import Data.Either (partitionEithers)
 import Data.Function ((&))
 import Data.List (sortOn)
 import Data.Map.Strict qualified as Map
 import Data.MemPack.Extra (ByteStringSized (..), CBORTerm (..))
-import Data.Proxy (Proxy (..))
 import Data.Set qualified as Set
-import Data.Text (Text)
 import Data.Text qualified as T
-import Data.Text.Encoding (decodeUtf8)
-import Data.Text.IO qualified as TIO
-import Data.TreeDiff.List (Edit (..), diffBy)
+import Data.TreeDiff.Tree (treeDiff)
 import GHC.TypeNats
+import Prettyprinter.Render.Terminal (putDoc)
 import Streaming qualified as S
 import Streaming.Internal qualified as S
 import Streaming.Prelude qualified as S
@@ -68,21 +66,6 @@ data DiffCmd = DiffCmd
   }
   deriving (Show, Eq)
 
-data DiffSide = SideFirst | SideSecond
-  deriving (Show, Eq)
-
-data DiffEntry
-  = NamespaceOnly DiffSide Namespace
-  | KeyOnly DiffSide Namespace ByteString
-  | ValueDiff Namespace ByteString CBORTerm CBORTerm
-  deriving (Show)
-
-data DiffLine
-  = LineBoth Text
-  | LineFirst Text
-  | LineSecond Text
-  deriving (Show, Eq)
-
 runDiffCmd :: (MonadLogger m, MonadUnliftIO m) => [(Namespace, Int)] -> DiffCmd -> m Result
 runDiffCmd namespaceKeySizes DiffCmd{..} = do
   namespacesFirst <- liftIO $ extractNamespaceList diffFirstFile
@@ -105,7 +88,11 @@ runDiffCmd namespaceKeySizes DiffCmd{..} = do
     let diffs =
           S.for
             (S.each namespaceKeySizesToCheck)
-            (\(ns, keySize) -> diffNamespace keySize diffVerbosity handleFirst handleSecond ns nsFirstSet nsSecondSet)
+            ( \(ns, keySize) ->
+                case someNatVal (fromIntegral keySize) of
+                  SomeNat p ->
+                    diffNamespace p diffVerbosity handleFirst handleSecond ns nsFirstSet nsSecondSet
+            )
     if not (null unknownNamespaces)
       then do
         when (diffVerbosity >= VerbosityNormal) do
@@ -113,7 +100,7 @@ runDiffCmd namespaceKeySizes DiffCmd{..} = do
         pure OtherError
       else do
         let filteredDiffs = if diffOnlyFirst then S.take 1 diffs else diffs
-        nDiffs <- emitDiffOutput diffDepth diffFirstFile diffSecondFile filteredDiffs
+        nDiffs <- emitDiffOutput diffDepth filteredDiffs
         when (diffVerbosity >= VerbosityNormal) $
           logInfoN $
             "Total differences found: " <> T.pack (show nDiffs)
@@ -126,25 +113,26 @@ selectNamespaces Nothing nsFirst nsSecond =
 selectNamespaces (Just nsList) nsFirst nsSecond =
   filter (\ns -> Set.member ns nsFirst || Set.member ns nsSecond) nsList
 
-diffNamespace :: (MonadIO m, MonadLogger m) => Int -> DiffVerbosity -> Handle -> Handle -> Namespace -> Set.Set Namespace -> Set.Set Namespace -> S.Stream (S.Of DiffEntry) m ()
-diffNamespace keySize verbosity handleFirst handleSecond ns nsFirst nsSecond =
+diffNamespace :: (MonadIO m, MonadLogger m, KnownNat n) => proxy n -> DiffVerbosity -> Handle -> Handle -> Namespace -> Set.Set Namespace -> Set.Set Namespace -> S.Stream (S.Of DiffEntry) m ()
+diffNamespace (_ :: proxy n) verbosity handleFirst handleSecond ns nsFirst nsSecond =
   case (Set.member ns nsFirst, Set.member ns nsSecond) of
-    (True, False) -> S.yield (NamespaceOnly SideFirst ns)
-    (False, True) -> S.yield (NamespaceOnly SideSecond ns)
+    (True, False) ->
+      S.yield (DelNamespace ns)
+    (False, True) ->
+      S.yield (InsNamespace ns)
     (False, False) -> pure ()
     (True, True) -> do
-      case someNatVal (fromIntegral keySize) of
-        SomeNat (Proxy :: Proxy n) -> do
-          when (verbosity >= VerbosityVerbose) do
-            S.lift $ logInfoN $ "Comparing namespace: " <> Namespace.asText ns
-          let streamFirst =
-                namespacedData @(ChunkEntry (ByteStringSized n) CBORTerm) handleFirst ns
-          let streamSecond =
-                namespacedData @(ChunkEntry (ByteStringSized n) CBORTerm) handleSecond ns
-          diffStreams
-            ns
-            streamFirst
-            streamSecond
+      when (verbosity >= VerbosityVerbose) do
+        S.lift $ logInfoN $ "Comparing namespace: " <> Namespace.asText ns
+      let streamFirst =
+            namespacedData @(ChunkEntry (ByteStringSized n) CBORTerm) handleFirst ns
+      let streamSecond =
+            namespacedData @(ChunkEntry (ByteStringSized n) CBORTerm) handleSecond ns
+      diffStreams
+        ns
+        streamFirst
+        streamSecond
+
 diffStreams ::
   (Monad m) =>
   Namespace ->
@@ -161,23 +149,28 @@ diffStreams ns = go
             k2 = entryKey e2
         case compare k1 k2 of
           LT -> do
-            S.yield (KeyOnly SideFirst ns k1)
+            S.yield (DelKey ns k1)
             go r1 (S.cons e2 r2)
           GT -> do
-            S.yield (KeyOnly SideSecond ns k2)
+            S.yield (InsKey ns k2)
             go (S.cons e1 r1) r2
           EQ -> do
             let v1 = chunkEntryValue e1
                 v2 = chunkEntryValue e2
             when (getEncodedBytes v1 /= getEncodedBytes v2) do
-              S.yield (ValueDiff ns k1 v1 v2)
+              let term1 = toFlatTerm $ encodeTerm $ getRawTerm v1
+                  term2 = toFlatTerm $ encodeTerm $ getRawTerm v2
+                  tree1 = termToTree term1
+                  tree2 = termToTree term2
+                  diff = treeDiff tree1 tree2
+              S.yield (SwpValueTree ns k1 diff)
             go r1 r2
       -- Only first stream has entries
       (S.Step (e1 S.:> r1), S.Return _) -> do
-        collectRemaining SideFirst e1 r1
+        collectRemaining DelKey e1 r1
       -- Only second stream has entries
       (S.Return _, S.Step (e2 S.:> r2)) -> do
-        collectRemaining SideSecond e2 r2
+        collectRemaining InsKey e2 r2
       -- One stream has an effect, apply it and continue
       (S.Effect m, _) ->
         S.Effect $ fmap (\str -> go str s2) m
@@ -186,64 +179,31 @@ diffStreams ns = go
       -- Both streams are done
       (S.Return _, S.Return _) -> pure ()
 
-  collectRemaining side entry rest = do
-    S.yield (KeyOnly side ns (entryKey entry))
-      <> S.map
-        (\e -> KeyOnly side ns (entryKey e))
-        rest
+  collectRemaining side entry =
+    S.map (side ns . entryKey) . S.cons entry
 
 entryKey :: ChunkEntry (ByteStringSized n) CBORTerm -> ByteString
 entryKey ChunkEntry{chunkEntryKey = ByteStringSized k} = k
 
-emitDiffOutput :: (MonadIO m) => DiffDepth -> FilePath -> FilePath -> S.Stream (S.Of DiffEntry) m () -> m Int
-emitDiffOutput depth fileFirst fileSecond diffs =
+emitDiffOutput :: (MonadIO m) => DiffDepth -> S.Stream (S.Of DiffEntry) m () -> m Int
+emitDiffOutput depth diffs =
   case depth of
     DepthSilent -> S.length_ diffs
-    DepthReference ->
-      S.mapM_ (liftIO . TIO.putStrLn . renderReference) $ S.length_ $ S.copy $ diffs
+    DepthReference -> do
+      diffs
+        & S.copy
+        & S.mapM_
+          ( \edit -> liftIO $ do
+              putDoc $ ppEditDiffEntry edit
+              putStrLn ""
+          )
+        & S.length_
     DepthFull ->
-      S.mapM_ (liftIO . renderFull fileFirst fileSecond) $ S.length_ $ S.copy $ diffs
-
-renderReference :: DiffEntry -> Text
-renderReference = \case
-  NamespaceOnly SideFirst ns -> "- " <> Namespace.asText ns
-  NamespaceOnly SideSecond ns -> "+ " <> Namespace.asText ns
-  KeyOnly SideFirst ns key -> "- " <> renderKeyRef ns key
-  KeyOnly SideSecond ns key -> "+ " <> renderKeyRef ns key
-  ValueDiff ns key _ _ -> "* " <> renderKeyRef ns key
-
-renderFull :: FilePath -> FilePath -> DiffEntry -> IO ()
-renderFull fileFirst fileSecond diffEntry =
-  case diffEntry of
-    ValueDiff ns key v1 v2 -> do
-      TIO.putStrLn $ "* " <> renderKeyRef ns key
-      TIO.putStrLn $ "--- " <> T.pack fileFirst
-      TIO.putStrLn $ "+++ " <> T.pack fileSecond
-      let linesFirst = prettyCBORLines v1
-      let linesSecond = prettyCBORLines v2
-      mapM_ (TIO.putStrLn . renderDiffLine) (diffLines linesFirst linesSecond)
-    _ -> TIO.putStrLn (renderReference diffEntry)
-
-renderKeyRef :: Namespace -> ByteString -> Text
-renderKeyRef ns key =
-  Namespace.asText ns <> "/" <> decodeUtf8 (Base16.encode key)
-
-prettyCBORLines :: CBORTerm -> [Text]
-prettyCBORLines term =
-  T.lines $ T.pack $ prettyHexEnc $ encodeTerm $ getRawTerm term
-
-renderDiffLine :: DiffLine -> Text
-renderDiffLine = \case
-  LineBoth t -> "  " <> t
-  LineFirst t -> "- " <> t
-  LineSecond t -> "+ " <> t
-
-diffLines :: [Text] -> [Text] -> [DiffLine]
-diffLines xs ys =
-  foldMap renderEdit (diffBy (==) xs ys)
- where
-  renderEdit = \case
-    Cpy t -> [LineBoth t]
-    Del t -> [LineFirst t]
-    Ins t -> [LineSecond t]
-    Swp t1 t2 -> [LineFirst t1, LineSecond t2]
+      diffs
+        & S.copy
+        & S.mapM_
+          ( \edit -> liftIO $ do
+              putDoc $ ppEditDiffEntry edit
+              putStrLn ""
+          )
+        & S.length_
---
+++ scls-util/src/Cardano/SCLS/Util/Diff/TermDiff.hs
@@ -0,0 +1,123 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Cardano.SCLS.Util.Diff.TermDiff (
+  termToTree,
+  Token (..),
+) where
+
+import Codec.CBOR.FlatTerm (FlatTerm, TermToken (..))
+import Data.ByteString qualified as BS
+import Data.Text qualified as T
+import Data.Tree (Tree (Node))
+import Data.Word (Word64, Word8)
+
+data Token
+  = TInt !Int
+  | TInteger !Integer
+  | TBytesLen !Word
+  | TByte !Word8
+  | TBytesBegin
+  | TStringLen !Word
+  | TString !T.Text
+  | TStringBegin
+  | TListLen !Word
+  | TListBegin
+  | TMapLen !Word
+  | TMapBegin
+  | TBreak
+  | TTag !Word64
+  | TBool !Bool
+  | TNull
+  | TSimple !Word8
+  | TFloat16 !Float
+  | TFloat32 !Float
+  | TFloat64 !Double
+  deriving (Eq, Show)
+
+termTokenToDiffTreeTokenNode :: TermToken -> Tree Token
+termTokenToDiffTreeTokenNode (TkInt n) = Node (TInt n) []
+termTokenToDiffTreeTokenNode (TkInteger n) = Node (TInteger n) []
+termTokenToDiffTreeTokenNode (TkBytes bs) = Node (TBytesLen (fromIntegral $ BS.length bs)) (map (flip Node [] . TByte) (BS.unpack bs))
+termTokenToDiffTreeTokenNode (TkString s) = Node (TStringLen (fromIntegral $ T.length s)) [Node (TString s) []]
+termTokenToDiffTreeTokenNode TkBytesBegin = Node TBytesBegin []
+termTokenToDiffTreeTokenNode TkStringBegin = Node TStringBegin []
+termTokenToDiffTreeTokenNode (TkListLen n) = Node (TListLen n) []
+termTokenToDiffTreeTokenNode TkListBegin = Node TListBegin []
+termTokenToDiffTreeTokenNode (TkMapLen n) = Node (TMapLen n) []
+termTokenToDiffTreeTokenNode TkMapBegin = Node TMapBegin []
+termTokenToDiffTreeTokenNode TkBreak = Node TBreak []
+termTokenToDiffTreeTokenNode (TkTag n) = Node (TTag n) []
+termTokenToDiffTreeTokenNode (TkBool b) = Node (TBool b) []
+termTokenToDiffTreeTokenNode TkNull = Node TNull []
+termTokenToDiffTreeTokenNode (TkSimple n) = Node (TSimple n) []
+termTokenToDiffTreeTokenNode (TkFloat16 f) = Node (TFloat16 f) []
+termTokenToDiffTreeTokenNode (TkFloat32 f) = Node (TFloat32 f) []
+termTokenToDiffTreeTokenNode (TkFloat64 f) = Node (TFloat64 f) []
+
+termToTree :: FlatTerm -> Tree Token
+termToTree [] = error "Unexpected end of tokens"
+termToTree (xt : ts) = go xt restEmptyOrError [] ts
+ where
+  go TkListBegin =
+    lenIndef TListBegin
+  go (TkListLen len) =
+    fixedLength (TListLen len) len
+  go TkBytesBegin =
+    lenIndef TBytesBegin
+  go TkStringBegin =
+    lenIndef TStringBegin
+  go TkMapBegin =
+    lenIndef TMapBegin
+  go (TkMapLen len) =
+    fixedLength (TMapLen len) len
+  go t = \f _ xs ->
+    f (termTokenToDiffTreeTokenNode t) xs
+
+  restEmptyOrError t [] = t
+  restEmptyOrError _ (_ : _) = error "Unexpected tokens"
+
+  lenIndef _ _ _ [] = error "Unexpected end of tokens in indefinite-length structure"
+  lenIndef token f acc (TkBreak : xs) = f (Node token (reverse acc)) xs
+  lenIndef token f acc (TkListBegin : xs) =
+    lenIndef TListBegin (lenIndef token f . flip (:) acc) [] xs
+  lenIndef token f acc (TkBytesBegin : xs) =
+    lenIndef TBytesBegin (lenIndef token f . flip (:) acc) [] xs
+  lenIndef token f acc (TkStringBegin : xs) =
+    lenIndef TStringBegin (lenIndef token f . flip (:) acc) [] xs
+  lenIndef token f acc (TkMapBegin : xs) =
+    lenIndef TMapBegin (lenIndef token f . flip (:) acc) [] xs
+  lenIndef token f acc (TkListLen len : xs) =
+    fixedLength (TListLen len) len (lenIndef token f . flip (:) acc) [] xs
+  lenIndef token f acc (TkMapLen len : xs) =
+    fixedLength (TMapLen len) len (lenIndef token f . flip (:) acc) [] xs
+  lenIndef token f acc (t : xs) =
+    lenIndef token f (termTokenToDiffTreeTokenNode t : acc) xs
+
+  fixedLength token@(TMapLen _) len f acc xs
+    | length acc == fromIntegral len * 2 =
+        f (Node token (reverse acc)) xs
+  fixedLength token len f acc xs
+    | length acc == fromIntegral len =
+        f (Node token (reverse acc)) xs
+  fixedLength _ _ _ _ [] =
+    error "Unexpected end of tokens in fixed-length structure"
+  fixedLength token len f acc (TkListBegin : xs) =
+    lenIndef TListBegin (fixedLength token len f . flip (:) acc) [] xs
+  fixedLength token len f acc (TkBytesBegin : xs) =
+    lenIndef TBytesBegin (fixedLength token len f . flip (:) acc) [] xs
+  fixedLength token len f acc (TkStringBegin : xs) =
+    lenIndef TStringBegin (fixedLength token len f . flip (:) acc) [] xs
+  fixedLength token len f acc (TkMapBegin : xs) =
+    lenIndef TMapBegin (fixedLength token len f . flip (:) acc) [] xs
+  fixedLength token len f acc (TkListLen l : xs) =
+    fixedLength (TListLen l) l (fixedLength token len f . flip (:) acc) [] xs
+  fixedLength token len f acc (TkMapLen l : xs) =
+    fixedLength (TMapLen l) l (fixedLength token len f . flip (:) acc) [] xs
+  fixedLength token len f acc (t : xs) =
+    fixedLength token len f (termTokenToDiffTreeTokenNode t : acc) xs
