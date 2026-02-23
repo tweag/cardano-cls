73e97e242eee131b52317d72fe81dde946fb53c6
João Santos Reis <joao.reis@tweag.io>
2026-02-23 16:49:06 +0000

    Enhance merkle tree test suite with additional property tests for leaf and node hashes

--- merkle-tree-incremental/test/Spec.hs
+++ merkle-tree-incremental/test/Spec.hs
@@ -1,7 +1,21 @@
-import Crypto.Hash.Algorithms (SHA3_256 (..))
-import Crypto.Hash.MerkleTree.Incremental (add, empty, finalize, merkleRootHash)
+{-# LANGUAGE OverloadedStrings #-}
+
+import Crypto.Hash.Algorithms (Blake2b_224 (..), SHA3_256 (..))
+import Crypto.Hash.MerkleTree.Incremental (
+  add,
+  empty,
+  finalize,
+  merkleRootHash,
+ )
+import Crypto.Hash.MerkleTree.Incremental.Internal (
+  leafHash,
+  nodeHash,
+ )
 import Reference (mkMerkleTree, mtHash)
 
+import Crypto.Hash (Digest, hash)
+import Data.ByteArray (unpack)
+import Data.ByteString (ByteString, pack, singleton)
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck.Instances.ByteString ()
@@ -16,3 +30,17 @@ main = do
               merkleTree1 = finalize state
               merkleTree2 = mkMerkleTree entries
           merkleRootHash merkleTree1 `shouldBe` mtHash merkleTree2
+      prop "leaf hash of (prefix, entry) should H(0x01 || prefix || entry)" $ do
+        \(prefix :: ByteString) (entry :: ByteString) -> do
+          let computedHash = leafHash prefix entry
+              expectedHash :: Digest Blake2b_224 = hash (singleton 0x01 <> prefix <> entry)
+          computedHash `shouldBe` expectedHash
+      prop "node hash of (left, right) should H(0x00 || H(left) || H(right))" $ do
+        \(left :: ByteString) (right :: ByteString) -> do
+          let leftHash = hash left :: Digest Blake2b_224
+              rightHash = hash right :: Digest Blake2b_224
+              computedHash = nodeHash leftHash rightHash
+              leftHashBytes = pack $ unpack leftHash
+              rightHashBytes = pack $ unpack rightHash
+              expectedHash = hash (singleton 0x00 <> leftHashBytes <> rightHashBytes) :: Digest Blake2b_224
+          computedHash `shouldBe` expectedHash
