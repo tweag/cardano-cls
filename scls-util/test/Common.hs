afb8f9364fbf47fbf7609f95620d6a6aab82c748
João Santos Reis <joaosreis@outlook.pt>
2026-03-30 14:42:05 +0100

    Run without cabal args

--- scls-util/test/Common.hs
+++ scls-util/test/Common.hs
@@ -48,6 +48,6 @@ runSclsUtil :: Maybe FilePath -> [String] -> IO (ExitCode, String, String)
 runSclsUtil Nothing args = do
   cabalPath <- lookupEnv "CABAL"
   let (cabalCmd, cabalArgs) = fromMaybe ("cabal", []) $ cabalPath >>= uncons . words
-  readProcessWithExitCode cabalCmd (cabalArgs ++ ["run", "scls-util", "--"] ++ args) ""
+  readProcessWithExitCode cabalCmd (["run", "scls-util", "--"] ++ args) ""
 runSclsUtil (Just path) args = do
   readProcessWithExitCode path args ""
