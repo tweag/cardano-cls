9e3a9e467768f289d2edddc44ff4779d27d7ff14
João Santos Reis <joao.reis@tweag.io>
2026-04-02 13:27:45 +0100

    Add entities/stake_pools/v0 namespace and definitions

---
+++ scls-cardano/cddl-src/Cardano/SCLS/Namespace/EntitiesStakePools.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use camelCase" #-}
+module Cardano.SCLS.Namespace.EntitiesStakePools where
+
+import Cardano.SCLS.Common
+import Codec.CBOR.Cuddle.Huddle
+import Data.Function (($))
+import Text.Heredoc (str)
+
+record_entry :: Rule
+record_entry =
+  comment
+    [str| The key for the namespace
+        |
+        | ```
+        | meta:
+        |   endian: be
+        |
+        | seq:
+        |   - id: keyhash_stakepool
+        |         doc: keyhash of the stake pool
+        |         size: 28
+        | ```
+        |]
+    $ "record_entry" =:= stake_pool
+
+stake_pool :: Rule
+stake_pool =
+  "stake_pool"
+    =:= mp
+      [ "stake_pool_state" ==> stake_pool_state / VNil
+      , "retiring_epoch_no" ==> epoch_no / VNil
+      ]
+
+stake_pool_state :: Rule
+stake_pool_state =
+  "stake_pool_state"
+    =:= mp
+      [ "vrf" ==> vrf_keyhash
+      , "cost" ==> coin
+      , "margin" ==> unit_interval
+      , "owners" ==> set staking_keyhash
+      , "pledge" ==> coin
+      , "relays" ==> arr [0 <+ a relay]
+      , "deposit" ==> coin
+      , "metadata" ==> pool_metadata / VNil
+      , "account_id" ==> account_id
+      , "delegators" ==> set credential
+      ]
