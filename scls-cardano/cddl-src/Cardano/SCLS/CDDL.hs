e25d2c57aef2f2d39e530e6729c3bc606cb4a31f
João Santos Reis <joao.reis@tweag.io>
2026-04-27 09:47:18 +0100

    Update gov/proposals/v0 namespace key size comment for clarity

--- scls-cardano/cddl-src/Cardano/SCLS/CDDL.hs
+++ scls-cardano/cddl-src/Cardano/SCLS/CDDL.hs
@@ -134,5 +134,5 @@ type instance Spec.NamespaceKeySize "entities/dormant_epochs/v0" = 1
 type instance Spec.NamespaceKeySize "gov/committee/v0" = 8
 type instance Spec.NamespaceKeySize "gov/constitution/v0" = 8
 type instance Spec.NamespaceKeySize "gov/pparams/v0" = 4
-type instance Spec.NamespaceKeySize "gov/proposals/v0" = 34 -- 32 bytes txid+tx index + 2 bytes for proposal index
+type instance Spec.NamespaceKeySize "gov/proposals/v0" = 34 -- 28 bytes txid, 4 bytes tx index, 2 bytes for proposal index
 type instance Spec.NamespaceKeySize "gov/proposals/roots/v0" = 1 -- 1 byte for proposal purpose
