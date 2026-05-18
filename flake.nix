3a2b55aa2f639520dfd3d6f76a8e79f976504f65
João Santos Reis <joao.reis@tweag.io>
2026-05-18 12:00:50 +0100

    Fix record_entry rule to use proposal directly

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
   inputs.cips = {
-    url = "github:tweag/CIPs?rev=8b1b619bef7e11b37f21e2b39f3159ea5d48af23";
+    url = "github:tweag/CIPs?rev=983b15e27d662535b34ca32851ada83ed0ccb395";
     flake = false;
   };
   outputs = { self, nixpkgs, flake-utils, haskellNix, git-hooks, treefmt-nix
--- scls-cardano/cddl-src/Cardano/SCLS/Namespace/GovProposals.hs
+++ scls-cardano/cddl-src/Cardano/SCLS/Namespace/GovProposals.hs
@@ -47,7 +47,7 @@ record_entry =
         | ```
         |
         |]
-    $ "record_entry" =:= arr [a word64, a proposal]
+    $ "record_entry" =:= proposal
 
 committee_cold_credential :: Rule
 committee_cold_credential = "committee_cold_credential" =:= credential
